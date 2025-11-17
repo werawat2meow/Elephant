21512e809a9971ed425e5185cba6c4a7addc9fc9
werawat2meow <werawat2meow@gmail.com>
2025-11-17 11:31:17 +0700

    fix code deploy

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,12 +1,8 @@
-// const config = {
-//   plugins: ["@tailwindcss/postcss"],
-// };
-
-// export default config;
-
-
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
 
 export default config;
